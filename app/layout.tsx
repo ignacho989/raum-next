11205a589c644f457a5ade1b6e69371b363926c3
ignacho989 <ignaciosantillan989@gmail.com>
2025-02-08 11:17:22 +0100

    Google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
 import localFont from "next/font/local";
 import Navbar from "./components/navbar/navbar";
 import { useState } from "react";
+import Script from "next/script";
 
 const stolzl = localFont({
   src: "../public/fonts/Stolzl Book.ttf",
@@ -34,6 +35,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-2MSQZ12ZP6`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2MSQZ12ZP6');
+  `}
+        </Script>
+      </head>
       <body className={`${stolzl.className} main-layout`}>
         <header>
           {/* <button className="boton-idioma">ES / EN</button> */}
